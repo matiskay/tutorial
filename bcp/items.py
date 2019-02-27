346f7a7b9471debbade2efc561aad8ce5f859f32
Edgar Marca <matiskay@gmail.com>
2019-02-27 17:43:49 -0500

    clean text

--- bcp/items.py
+++ bcp/items.py
@@ -6,11 +6,23 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import re
+
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Join
 
 from w3lib.html import remove_tags
 
+_clean_spaces_re = re.compile(' +', re.U)
+
+
+def clean_spaces(value):
+    return _clean_spaces_re.sub(' ', value)
+
+
+def clean_html(html):
+    html = html.replace('\xa0', ' ')
+    return html
 
 class NewsItem(scrapy.Item):
     # define the fields for your item here like:
@@ -24,6 +36,6 @@ class NewsItemLoader(ItemLoader):
     default_item_class = NewsItem
 
     default_output_processor = TakeFirst()
-    default_input_processor = MapCompose(remove_tags, str.strip)
+    default_input_processor = MapCompose(clean_spaces, clean_html, remove_tags, str.strip)
 
     content_out = Join('\n')
